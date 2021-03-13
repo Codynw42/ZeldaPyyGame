b866663c892ead92f6290a42bdb89658941df306
Codynw42 <79997729+Codynw42@users.noreply.github.com>
2021-03-13 14:24:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 
 
+
 FPS = 60
 WHITE = (255, 255, 255)
 WIDTH, HEIGHT = 1400, 800
@@ -17,32 +18,38 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 TEMPLE = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'Temple_Time.jpeg')), (WIDTH, HEIGHT))
 
 LINK_WIDTH, LINK_HEIGHT = 520, 330
-LINK_IMAGE = pygame.image.load(os.path.join('Assets', 'Link_Face_Right.png'))
+LINK_IMAGE = pygame.image.load(os.path.join('Assets', 'Link_Standing.png'))
 WalkLeft = pygame.image.load(os.path.join('Assets', 'Link_Face_Left.png'))
 WalkRight = pygame.image.load(os.path.join('Assets', 'Link_Face_Right.png'))
 LINK = pygame.transform.rotate(pygame.transform.scale(LINK_IMAGE, (LINK_WIDTH, LINK_HEIGHT)), 0) 
+Link_Left = pygame.transform.rotate(pygame.transform.scale(WalkLeft, (LINK_WIDTH, LINK_HEIGHT)), 0) 
+Link_Right = pygame.transform.rotate(pygame.transform.scale(WalkRight, (LINK_WIDTH, LINK_HEIGHT)), 0) 
 
 
-def draw_surface(link, WalkLeft, WalkRight, walkCount, left, right):
-    WIN.fill((WHITE))
+def draw_surface(link, WalkLeft, WalkRight, left, right):
+    #WIN.fill((WHITE))
     WIN.blit(TEMPLE, (0, 0))
-    #WIN.blit(LINK, (link.x, link.y))
+    WIN.blit(LINK, (link.x, link.y))
+
+    global walkCount
+    
     if walkCount + 1 >= 27:
         walkCount = 0
     if left:
-        WIN.blit(WalkLeft[walkCount//3], (link.x, link.y))
+        WIN.blit(Link_Left[walkCount//3], (link.x, link.y))
         walkCount += 1
     elif right:
-        WIN.blit(WalkRight[walkCount//3], (link.x, link.y))
+        WIN.blit(Link_Right[walkCount//3], (link.x, link.y))
         walkCount += 1
     else: WIN.blit(LINK, (link.x, link.y))
+
     
     
     pygame.display.update() 
 
 
 
-def link_movement(keys_pressed, link, jumpCount, isJump, walkCount, left, right):
+def link_movement(keys_pressed, link, jumpCount, isJump, left, right):
     if keys_pressed[pygame.K_a]:            #left
         link.x -= VEL
         left = True
@@ -78,9 +85,6 @@ def link_movement(keys_pressed, link, jumpCount, isJump, walkCount, left, right)
 
 
 
-
-
-
 def mainloop():
     link = pygame.Rect(60, 500, LINK_WIDTH, LINK_HEIGHT)                     
     
@@ -95,8 +99,8 @@ def mainloop():
 
         
         keys_pressed = pygame.key.get_pressed()          #you must use this code to be able to press multiple keys at a time.
-        link_movement(keys_pressed, link, isJump, jumpCount, walkCount, left, right)
-        draw_surface(link, WalkLeft, WalkRight, walkCount, left, right)
+        link_movement(keys_pressed, link, isJump, jumpCount, left, right)
+        draw_surface(link, WalkLeft, WalkRight, left, right)
         
         
        
